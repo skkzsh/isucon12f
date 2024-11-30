7cba5ff83083583b686eacfeb2c4ab39fdd718e1
skkzsh <skkzsh12@gmail.com>
2024-11-30 20:31:07 +0900

    userid_idxをDROPしないでみる #1

--- sql/setup/create_index_user_presents.sql
+++ sql/setup/create_index_user_presents.sql
@@ -1,2 +1,2 @@
-DROP INDEX userid_idx ON user_presents;
+-- DROP INDEX userid_idx ON user_presents;
 CREATE INDEX idx_user_presents_user_id_deleted_at_created_at ON user_presents (user_id, deleted_at, created_at DESC, id);
