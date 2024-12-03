2a9fa59860b5c63e61cf13cd1af4b8b83d7f93ae
skkzsh <skkzsh12@gmail.com>
2024-12-03 20:28:30 +0900

    CREATE INDEX user_present_all_received_history #1

--- sql/3_schema_exclude_user_presents.sql
+++ sql/3_schema_exclude_user_presents.sql
@@ -132,6 +132,8 @@ CREATE TABLE `user_present_all_received_history` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+CREATE INDEX `idx_user_present` ON user_present_all_received_history (`user_id`, `present_all_id`);
+
 /* ガチャマスタ */
 
 CREATE TABLE `gacha_masters` (
